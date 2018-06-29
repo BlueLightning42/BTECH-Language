9ec66f62c4b5477c41b912ecba21f6e56ebf14ef
Blue Ϟ Lightning <jawg45@live.com>
2018-06-29 11:13:05 -0400

    Finished Parser
    Unstil I test it and fix all the bugs/dumb logic

--- Source/paser.cpp
+++ Source/paser.cpp
@@ -1,30 +1,45 @@
 #include "BTECH.h"
-token EndOfFile;
+token EndOfLine("EOL"), EndOfFile("EOF");
+std::set <char> ops = {'+','-','/','*','%','&','(',')','>','<','^','|','=','!'};
 program::program(){
-        //parse input into token array
-        std::string file;
-        for (std::string::iterator c = file.begin(); c++) {
-            if (*c == ' ' || *c == '	') { continue; }
+    // parse input into token array
+    std::string file;
+    for (std::string::iterator c = file.begin(); c++) {
+        if (*c == '"' || *c == '\'') {  // build strings
+            c++;
+            unsigned dlim_start = *c;
+            while (!(*c == '"' || *c == '\'') && (*c-1 == '\\')) {c++;}  // escaped quotes
 
-            if (*c == '"' || *c == '\'') {//build strings
-                c++;
-                unsigned dlim_start = *c;
-                while ((*c == '"' || *c == '\'') && (*c-1 != '\\')) {c++;} //escaped quotes
-                
-                _string s(file.substr(dlim_start, c));
+            _string s(file.substr(dlim_start, c));
                 tokens.push_back(s);
-            }
-
-            if (*c == '\n' && *c-1 != '\\') {
-                tokens.push_back(EndOfFile);
-                return;
-            }
-
-            if (*c == '+' || *c == '-' || *c == '*' || *c == '/') {
-                _operator op(*c);
-            }
-
         }
-    }
+        else if (*c == '\n' && *c-1 != '\\') {  // end of commands
+            tokens.push_back(EndOfLine);
+            return;
+        }
+        else if (ops.find(*c) != ops.end()) {  // build operators
+            _operator op(*c);
+        }
+        else  if (*c == '.' || *c == 'i' || isdigit(c*)){  // build numbers
+            unsigned dlim_start = *c;
+            while (!(*c == ' ' || *c == '	' || c* == '\n' || ops.find(*c)) {c++;}  // escaped quotes not space newline or operator
 
+           std::string temp = (file.substr(dlim_start, c);
+                if (temp[0] == 'i'){
+                    tokens.push_back(imaginary(std::stod(temp));
+                }else if (temp.find('.')){
+                    tokens.push_back(_double(std::stod(temp));		   	
+                }else{
+                    tokens.push_back(std::stoi(temp));
+                }
+        }
+        else{  // build everything else
+            unsigned dlim_start = *c;
+            while (!(*c == ' ' || *c == '	' || c* == '\n' || ops.find(*c)) {c++;}
+
+            token t(file.substr(dlim_start, c));
+            tokens.push_back(t);
+        }
+    tokens.push_back(EndOfFile);
+    }
 };
