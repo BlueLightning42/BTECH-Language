5c74961187b9fbe99105031d84372b4822dca1a0
Blue Ϟ Lightning <jawg45@live.com>
2018-07-10 11:15:08 -0400

    Start Setting up Printing tokens
    note:
     unable to test implementation until I fix the template for numbers :/

--- Source/paser.cpp
+++ Source/paser.cpp
@@ -3,10 +3,45 @@
 #include <fstream>
 #include <iostream>
 
+using namespace BTECH;
 
 std::set <char> ops = {'+','-','/','*','%','&','(',')','>','<','^','|','=','!'};
-_string::_string(std::string c): token("string"), contents(c) {}
+//token initializers
+token::token(std::string s): name(s) {}
+//default Unknown
+_generic_token::_generic_token(std::string n="Unknown Token"): token(n) {} 
+_string::_string(std::__cxx11::string c): token("string"), contents(c) {}
+_operator::_operator(char t): token("op"), _type(t) {}
+_number::_number(N n): token("number"), value(n) {}
+_number::get_value() const {return value} //getter for numbers
 
+//boilerplate for printing tokens?
+std::ostream& operator<< (std::ostream& os, const token& tok) {
+    tok.print(os); //call print method of derived class
+    return os;
+}
+void _generic_token::print(std::ostream& os) const{
+	os << this->name << " ";
+	if (this->name == "EOL"){
+		os << std::endl;
+	}
+}
+void _operator::print(std::ostream& os) const{
+	os <<this->name << "(" << this->_type << ") ";
+}
+void _string::print(std::ostream& os) const{
+	os << this->name << " \"" << this->contents << "\"";
+}
+void _number::print(std::ostream& os) const{
+	os << this->get_value() << " ";
+}
+// etc...
+
+
+
+
+
+//first step is the parser will tokenize the file
 void program::build_program(std::string f){
 	// parse input into token array
 
@@ -21,11 +56,12 @@ void program::build_program(std::string f){
 			// comment out whole line
 			file.ignore(10000,'\n');
 
-		}else if (file.peek() == '"') {  // build strings
+		}else if (file.peek() == '"') {  // build strings "
+			file.ignore(1);//ignore first quote
 			getline(file, line,'"');
 			tokens.push_back(new _string(line));
 
-		}else if (file.peek() == '\'') {  // build strings
+		}else if (file.peek() == '\'') {  // build strings '
 			getline(file, line,'\'');
 			tokens.push_back(new _string(line));
 
@@ -36,25 +72,37 @@ void program::build_program(std::string f){
 			tokens.push_back(new _operator(file.get()));
 
 		}else if (file.peek() == 'i' && isdigit(file.peek()+1)) {  // build operators
-			tokens.push_back(new _operator(file.get()));
+			getline(file, line,' ');
+			tokens.push_back(new _number(line)); //TODO figure out complex later
 
 		}else  if (file.peek() == '.' || isdigit(file.peek())){
 		// build numbers
 			getline(file, line,' ');
 			if ( line.find('.') != std::string::npos){//contains a decimal
-				tokens.push_back(new _double(std::stod(line));	
+				tokens.push_back(new _number(std::stod(line)));	
 			}else{
-				tokens.push_back(new std::stoi(line));
+				tokens.push_back(new _number(std::stoi(line)));
 			}
 
 		}else{  // build everything else
 			getline(file, line,' ');
-			tokens.push_back(new token(line));
+			tokens.push_back(new _generic_token(line));
 
 		}
-
+		tokens.push_back(new _generic_token("EOL"));
 	}file.close();
-	tokens.push_back(new token("EOF"));
-	std::cout << "Tokenizing sucessfull";
+
+	tokens.push_back(new _generic_token("EOF"));
+
+	if(debug){
+		std::cout << "Tokenizing sucessfull\nTokens generated:\n";
+		for(std::vector<token *>::iterator i = tokens.begin(); i != tokens.end(); i++){
+			if (/**/"" == "EOL"){ 
+				std::cout << "EOL\n";
+				continue;
+			}
+			//std::cout << i << "  ";
+		}
+	}
 	build_ast();
 };
