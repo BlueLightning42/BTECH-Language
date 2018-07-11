5aa7e52e79006f5c15736ae8af286961c990afe1
Blue Ϟ Lightning <jawg45@live.com>
2018-07-11 16:48:35 -0400

    structure change
    Trying to figure out how I want to structrue the langauge

--- Source/ast.cpp
+++ Source/ast.cpp
@@ -1,82 +1,81 @@
 #include "BTECH.h"
+using namespace BTECH;
+
+command::command(std::string s): name(s) {}
+function::function(std::string s): command(s) {}
+
+
+void program::build_ast(){
+	_itter = -1; // reause itterator
+	ast.push_back(new function("main"));
+	tokens.clear();//delete token tree;
+	optomise_ast();
+}
+
+
+
+
+
 //split vector of tokens created in parser into a tree of statements...
 //stuff like   func_init: name
-//             expression: number, operator, number
+//			 expression: number, operator, number
 
-program::build_ast(){
-    ast.push_back(function("main"));
-}
 function::function(std::string n) name(n){
-    _itter++;  // move global itterator past the ":"
-    if (tokens[*_itter+1].name_is(":"){
-        _cmd = function(get_token());  // nested functions
-    }else if (tokens[*_itter+1].name_is("("){
-       _itter++;
-        _cmd = build_multiline_command();
-    }else{
-        _cmd.push_back(build_command());
-    }
-   // return self; 
+	_itter++;  // move global itterator past the ":"
+	if (tokens[_itter+1].name_is(":"){
+		cmd = function(get_token());  // nested functions
+	}else if (tokens[_itter+1].name_is("("){
+		_itter++;
+		cmd = build_multiline_command();
+	}else{
+		cmd = build_command;
+	}
+	// return self; 
 }
 
 // is this...the factory pattern?
 command build_command(){
   command cmd;
   if (tokens[*_itter+1].name_is("=")){
-      cmd =  assignment(get_token());
+	  cmd =  assignment(get_token());
   }else if (tokens[*_itter+1].name_is("op")){  // type is instead
-      cmd =  expression();
+	  cmd =  expression();
   }else if (tokens[*_itter+1].name_is("{")){
-      cmd =  function_declariation(get_token());
+	  cmd =  function_declariation(get_token());
   }else if(tokens[*_itter+1].name_is(":")){
-      cmd = function(get_token());  // nested function calls
-  }else if(tokens[*_itter+1].name_is("#") ||tokens[*_itter+1].name_is("EOF") ){
-  //skip empty lines and comments
-  __itter++
-  cmd = build_command();
+	  cmd = function(get_token());  // nested function calls
+  }else if(tokens[*_itter+1].name_is("#") || tokens[*_itter+1].name_is("EOF") ){
+	//skip empty lines and comments
+	__itter++
+	cmd = build_command();
   }else{
-       std::cout << "Not a valid command";
+	   std::cout << "Not a valid command";
   }
   return cmd;
 }
 command build_multiline_command(){
-    std::vector <command> cmds;
-    while(  !(tokens[*_itter].name_is(")")){  // peek
-        cmds.push_back(build_command());
-    }
-    return cmds;
+	std::vector <command> cmds;
+	while(  !(tokens[*_itter].name_is(")")){  // peek
+		cmds.push_back(build_command());
+	}
+	return cmds;
 }
 function_declariation(token n){
    // make your own function and append to the global function list in the form
    /**
    custom_func{ (passed arguments-default *arg)
-                             (logic to be preformed on arg)
-                             (any returned variables)
+							 (logic to be preformed on arg)
+							 (any returned variables)
    }
    **/
    // note this is a joke language and creating functions is obtuse/hidden away like this because
    // its designed for you to use all the "custom" functions defined in the standard library (like yotka:)
    /**psudocode for yotka function would be
    yotka{ (expression e)
-               (a = cast_to_string((eval(e)))
-               (a)
-    }
-    **/
+			   (a = cast_to_string((eval(e)))
+			   (a)
+	}
+	**/
   }
 
-// NUM SUB NUM MULT NUM ADD NUM DIV EXPRESSION      //math
-class expression: command{
-    std::vector <tokens> expr;
-    command(){
-      while(  !(tokens[*_itter].name_is("EOF") || !(tokens[*_itter].name_is("#")){  // peek
-        if ((tokens[*_itter].type_is("number") || (tokens[*_itter].type_is("operator")){
-             expr.push_back(get_token());
-         }
-         expr.push_back(build_command());
-      }
-   }
-}
-class function: command{
-    std::string name;
-    function::function(std::string n);
-}
+// NUM SUB NUM MULT NUM ADD NUM DIV EXPRESSION	  //math
