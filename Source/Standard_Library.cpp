05e3fecaadcffb2d477560b9b1cbf71210159ab7
Blue Ϟ Lightning <jawg45@live.com>
2018-08-23 16:35:22 -0400

    Update Standard_Library.cpp

--- Source/Standard_Library.cpp
+++ Source/Standard_Library.cpp
@@ -20,13 +20,15 @@ void zen(	std::shared_ptr<token>&,	scope&);
 void _if(	std::shared_ptr<token>&,	scope&);
 void loop(	std::shared_ptr<token>&,	scope&);
 void redef(	std::shared_ptr<token>&,	scope&);
-std::map<std::string, BTECHFUNC> standard_functions ={
+const std::map<std::string, BTECHFUNC> standard_functions ={
 	{"yotka", 			&yotka},
 	{"jen", 			&jen},
+	{"jennifer", 		&jen},
 	{"zen", 			&zen},
 	{"nasim", 			&nasim},
 	{"if", 				&_if},
 	{"reassignment", 	&redef},
+	{"redef", 			&redef},
 	{"loop", 			&loop}
 };
 std::map<std::string, BTECHFUNC> custom_functions;
@@ -37,12 +39,13 @@ void call_function(std::string name, std::shared_ptr<token>& params, scope& curr
 	
 	auto std_func = standard_functions.find(name);
 	auto cust_func = custom_functions.find(name);
-	if (std_func != standard_functions.end()){
+
+	if (cust_func != custom_functions.end()){//custom functions have higher priority then standard
+		if (debug > 2) std::cout << "Called Custom Function: " <<name<<" With -> " << *params << std::endl;
+		cust_func->second(params, current_scope);
+	}else if (std_func != standard_functions.end()){
 		if (debug > 2) std::cout << "Called Standard Function: " <<name<<" With -> " << *params << std::endl;
 		std_func->second(params, current_scope);
-	}else if (cust_func != custom_functions.end()){
-		if (debug > 2) std::cout << "Called Custom Function: " <<name<<" With -> "  << *params << std::endl;
-		cust_func->second(params, current_scope);
 	}else{
 		if (debug > 2) std::cout << "Called UNFOUND FUNCTION EXITING" << std::endl;
 		auto tmp = std::make_shared<expression>("ERROR");
@@ -58,12 +61,12 @@ bool program::run_program(){
 	this->_main.run(result,scp, this->debug);
 
 	if (result->name_is("ERROR")){
-		if (this->debug > 2) std::cout << ".\n->========================== ERROR ===========================\n  ";
+		if (this->debug > 2) std::cout << ".\n->========================== ERROR ===========================\n";
 		std::dynamic_pointer_cast<expression>(result)->out_print();
 		std::cout << "\n==============================================================";
 		return 1;
 	}/*else*/
-	if (this->debug > 2) std::cout << ".\n->========================= Output ===========================\n  ";
+	if (this->debug > 2) std::cout << ".\n->========================= Output ===========================\n";
 	std::dynamic_pointer_cast<expression>(result)->out_print();
 	return 0;
 }
@@ -137,13 +140,19 @@ void jen(std::shared_ptr<token>& params, scope& current_scope){
 	std::stringstream tmp;
 	try{
 		params->jen_print(tmp, current_scope);
-	}catch(std::exception e){
+	}catch(std::exception e){//doesn't do shit...
 		tmp << "jen is confused\n";
 	}
+	for(auto it: current_scope.pointers){
+		if(it->name_is("nl")){ //done this way so you can overwrite the standard new line object
+			it->jen_print(tmp, current_scope);
+		}
+	}
 	params = std::make_shared<_string>(tmp.str());
+
 }
 void pointer::run(std::shared_ptr<token>& result, scope& current_scope, int debug) {
-	contents->run(result,current_scope, debug);
+	contents->run(result, current_scope, debug);
 } //forward
 void command::run(std::shared_ptr<token>& result, 	 scope& c, int d) {} //forward
 void expression::run(std::shared_ptr<token>& result, scope& c, int d) {} //forward
@@ -155,15 +164,15 @@ void scope::run(std::shared_ptr<token> &top_result, scope& current_scope, int de
 	if (debug>2) std::cout << "Entered Scope: "<< this->name <<std::endl;
 	auto result = std::make_shared<token>("NOP");
 	for(auto i: this->body){
-	   if (i != nullptr){
-		   i->run(result,*this, debug);
-		   if (result->name_is("string")){
-			  std::dynamic_pointer_cast<expression>(top_result)->body.push_back(result);
-		   }else if (result->name_is("ERROR")){
-			   top_result = result;
-			   return; //break out in the case of a error
-		   }
-	   }
+		if (i != nullptr){
+			i->run(result,*this, debug);
+			if (result->name_is("string")){
+				std::dynamic_pointer_cast<expression>(top_result)->body.push_back(result);
+			}else if (result->name_is("ERROR")){
+				top_result = result;
+				return; //break out in the case of a error
+			}
+		}
 	}
-	if (debug>2)  std::cout << "Scope: "<< this->name << " Exited" <<std::endl;
+	if (debug>2) std::cout << "Scope: "<< this->name << " Exited" <<std::endl;
 }
